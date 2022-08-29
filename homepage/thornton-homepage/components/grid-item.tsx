6b51217ec6877b3566e498fe3ed43a3fa264d1cb
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-08-28 22:06:44 -0400

    Add images to link cards

--- homepage/thornton-homepage/components/grid-item.tsx
+++ homepage/thornton-homepage/components/grid-item.tsx
@@ -1,6 +1,9 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, Suspense } from 'react'
+import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 import { Service } from '../interfaces/home.interfaces';
+
 
 type Props = {
   service: Service
@@ -11,6 +14,9 @@ const GridItem: FunctionComponent<Props> = ({ service }) => {
   return ( 
     <a href={service.url} className={styles.card}>
       <h2>{service.name} &rarr;</h2>
+      {service.img_src && (
+        <Image src={service.img_src} alt={service.name} width={100} height={100}/>
+      )}
       <p>{service.description}</p>
     </a>
   )
--- homepage/thornton-homepage/interfaces/home.interfaces.tsx
+++ homepage/thornton-homepage/interfaces/home.interfaces.tsx
@@ -1,5 +1,6 @@
 export interface Service {
-  name: string
-  description: string
-  url: string
+  name: string;
+  description: string;
+  url: string;
+  img_src?: string;
 }
