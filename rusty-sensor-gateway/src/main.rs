c581e2a0bf7596e1662924580ba6a1e46cd986d0
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-10-31 22:51:22 -0400

    Set up axum server

--- rusty-sensor-gateway/src/main.rs
+++ rusty-sensor-gateway/src/main.rs
@@ -1,24 +1,64 @@
-#[macro_use]
-extern crate rocket;
+use axum::{
+    extract::Json,
+    handler::Handler,
+    routing::get,
+    routing::post,
+    Router
+};
+use std::net::SocketAddr;
+use serde::Deserialize;
+use tower_http::cors::{Any, CorsLayer};
 
-#[macro_use]
-extern crate rocket_include_static_resources;
-
-static_response_handler! {
-    "/favicon.ico" => favicon => "favicon"
+#[derive(Deserialize, Debug)]
+pub struct Metric {
+  metric_name: String,
+  description: String,
+  metric_type: String,
+  data: f64
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[tokio::main]
+async fn main() {
+    // Let anything through since this only runs locally
+    let cors = CorsLayer::new().allow_origin(Any);
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/receive_metrics", post(receive_metrics))
+        .layer(cors);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .attach(static_resources_initializer!(
-            "favicon" => "src/static/images/favicon.ico"
-        ))
-        .mount("/", routes![favicon])
-        .mount("/", routes![index])
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Expect shutdown signal handler");
+    println!("signal shutdown");
+}
+
+//
+// Routes
+//
+
+async fn root() -> &'static str {
+    "Welcome to the Sensor Gateway - Now a bit rustier."
+}
+
+pub async fn receive_metrics(payload: String) -> String {
+    let metrics: Vec<Metric> = match serde_json::from_str(payload.as_str()) {
+        Ok(result) => result,
+        Err(error) => return format!("ERROR: {}", error)
+    };
+
+    println!("stuff: {:#?}", metrics);
+
+    "Your metrics have been received. Thanks!".to_string()
 }
